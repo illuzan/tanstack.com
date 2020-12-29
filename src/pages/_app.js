c0be79fabb02d26f0cff76084aa72efe3fc8f265
Tanner Linsley <tannerlinsley@gmail.com>
2020-12-29 11:19:32 -0700

    with-twind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import Head from 'next/head'
-import { GlobalStyles, theme } from 'twin.macro'
-import { createGlobalStyle } from 'styled-components'
-import '../styles.css'
+import { setup, tw } from 'twind'
+import { css } from 'twind/css'
 import { Seo } from '../components/Seo'
+import twindConfig from '../twind.config'
+
+if (typeof window !== 'undefined') {
+  setup(twindConfig)
+}
 
 // function loadScript(src, attrs = {}) {
 //   if (typeof document !== 'undefined') {
@@ -14,30 +17,30 @@ import { Seo } from '../components/Seo'
 //     script.src = src
 //     document.body.appendChild(script)
 //   }
-// }
+// }  <div className={tw(css({ '& *': 'z-10' }))} />
 
-const MyGlobalStyles = createGlobalStyle`
-  @media (prefers-color-scheme: dark) {
-    * {
-      scrollbar-color: ${theme`colors.gray.700`} ${theme`colors.gray.800`};
+// const MyGlobalStyles = createGlobalStyle`
+//   @media (prefers-color-scheme: dark) {
+//     * {
+//       scrollbar-color: ${theme`colors.gray.700`} ${theme`colors.gray.800`};
 
-      ::-webkit-scrollbar, scrollbar {
-        width: 1rem;
-        height: 1rem;
-      }
+//       ::-webkit-scrollbar, scrollbar {
+//         width: 1rem;
+//         height: 1rem;
+//       }
 
-      ::-webkit-scrollbar-track, scrollbar-track {
-        background: ${theme`colors.gray.800`};
-      }
+//       ::-webkit-scrollbar-track, scrollbar-track {
+//         background: ${theme`colors.gray.800`};
+//       }
 
-      ::-webkit-scrollbar-thumb, scrollbar-thumb {
-        background: ${theme`colors.gray.700`};
-        border-radius: .5rem;
-        border: 3px solid ${theme`colors.gray.800`};
-      }
-    }
-  }
-`
+//       ::-webkit-scrollbar-thumb, scrollbar-thumb {
+//         background: ${theme`colors.gray.700`};
+//         border-radius: .5rem;
+//         border: 3px solid ${theme`colors.gray.800`};
+//       }
+//     }
+//   }
+// `
 
 function MyApp({ Component, pageProps }) {
   // React.useEffect(() => {
@@ -48,9 +51,34 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {/* <style
+        dangerouslySetInnerHTML={{
+          __html: `
+            * {
+              scrollbar-color: ${theme.backgroundAlt} ${theme.gray};
+            }
+              
+            *::-webkit-scrollbar, scrollbar {
+              width: 1rem;
+              height: 1rem;
+            }
+            
+            *::-webkit-scrollbar-track, scrollbar-track {
+              background: ${theme.backgroundAlt};
+            }
+             
+            *::-webkit-scrollbar-thumb, scrollbar-thumb {
+              background: ${theme.gray};
+              border-radius: .5rem;
+              border: 3px solid ${theme.backgroundAlt};
+            }
+          `,
+        }}
+      /> */}
+      <div className={tw(css({ '& *': 'bg-blue-500' }))} />
       <Seo title="Quality Softare & Open Source Libraries for the Modern Web" />
-      <GlobalStyles />
-      <MyGlobalStyles />
+      {/* <GlobalStyles /> */}
+      {/* <MyGlobalStyles /> */}
       <Component {...pageProps} />
     </>
   )
